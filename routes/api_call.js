e49849533aa0dd518d72af16d0632c25b4d46639
Lê Gia Hoàng <giahoangth@gmail.com>
2017-09-25 22:13:57 +0700

    return all invoice

--- routes/api_call.js
+++ routes/api_call.js
@@ -1,46 +1,46 @@
-var tools = require("../tools/tools.js");
-var config = require("../config.json");
-var request = require("request");
-var express = require("express");
+var tools = require('../tools/tools.js');
+var config = require('../config.json');
+var request = require('request');
+var express = require('express');
 var router = express.Router();
-let redis = require("redis");
+let redis = require('redis');
 let client = redis.createClient();
-client.on("connect", () => {
-  console.log("Redis connected in api_call.js ");
+client.on('connect', () => {
+  console.log('Redis connected in api_call.js ');
 });
 /** /api_call **/
-router.get("/", function(req, res) {
+router.get('/', function(req, res) {
   let accessToken, realmId;
   let customerId;
-  client.get("accessToken", (err, reply) => {
+  client.get('accessToken', (err, reply) => {
     accessToken = reply;
-    client.get("realmId", (err, reply) => {
+    client.get('realmId', (err, reply) => {
       realmId = reply;
-      if (!accessToken) return res.json({ error: "Not authorized" });
+      if (!accessToken) return res.json({ error: 'Not authorized' });
       if (!realmId)
         return res.json({
           error:
-            "No realm ID.  QBO calls only work if the accounting scope was passed!"
+            'No realm ID.  QBO calls only work if the accounting scope was passed!'
         });
 
       // Set up API call (with OAuth2 accessToken)
-      let url = config.api_uri + realmId + "/query?query=select * from Invoice";
-      
-      console.log("req.query.id value: " + req.query.id);
+      let url = config.api_uri + realmId + '/query?query=select * from Invoice';
+
+      console.log('req.query.id value: ' + req.query.id);
       if (
-        typeof req.query.id !== "undefined" &&
+        typeof req.query.id !== 'undefined' &&
         /^[0-9]+$/.test(req.query.id)
       ) {
-        customerId = req.query.id;        
-        console.log("customerId value:" + customerId);
+        customerId = req.query.id;
+        console.log('customerId value:' + customerId);
       }
 
-      console.log("Making API call to: " + url);
+      console.log('Making API call to: ' + url);
       var requestObj = {
         url: url,
         headers: {
-          Authorization: "Bearer " + accessToken,
-          Accept: "application/json"
+          Authorization: 'Bearer ' + accessToken,
+          Accept: 'application/json'
         }
       };
 
@@ -58,23 +58,35 @@ router.get("/", function(req, res) {
             // let filterInvoices = Invoices.filter(object=>{
             //   if(typeof object.CustomerRef.value === "undefined"){
             //     return false;
-            //   } 
+            //   }
             //   if(typeof customerId !== "undefined") {
             //     return object.CustomerRef.value.toString() === customerId.toString();
             //   }
             //   return true;
             // });
             //way 2:
-            body.QueryResponse.Invoice = body.QueryResponse.Invoice.filter(object=>{
-              if(typeof object.CustomerRef.value === "undefined"){
-                return false;
-              } 
-              if(typeof customerId !== "undefined") {
-                return object.CustomerRef.value.toString() === customerId.toString();
+            body.QueryResponse.Invoice = body.QueryResponse.Invoice.filter(
+              object => {
+                if (typeof object.CustomerRef.value === 'undefined') {
+                  return false;
+                }
+                if (
+                  typeof customerId !== 'undefined' &&
+                  customerId.toString() === 'all' &&
+                  object.Balance > 0
+                ) {
+                  return true;
+                }
+                if (typeof customerId !== 'undefined') {
+                  return (
+                    object.CustomerRef.value.toString() ===
+                    customerId.toString()
+                  );
+                }
+                return true;
               }
-              return true;
-            });
-            
+            );
+
             // API Call was a success!
             res.json(body);
           },
@@ -89,19 +101,19 @@ router.get("/", function(req, res) {
 });
 
 /** /api_call/revoke **/
-router.get("/revoke", function(req, res) {
+router.get('/revoke', function(req, res) {
   var token = tools.getToken(req.session);
-  if (!token) return res.json({ error: "Not authorized" });
+  if (!token) return res.json({ error: 'Not authorized' });
 
   var url = tools.revoke_uri;
   request(
     {
       url: url,
-      method: "POST",
+      method: 'POST',
       headers: {
-        Authorization: "Basic " + tools.basicAuth,
-        Accept: "application/json",
-        "Content-Type": "application/json"
+        Authorization: 'Basic ' + tools.basicAuth,
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         token: token.accessToken
@@ -112,7 +124,7 @@ router.get("/revoke", function(req, res) {
         return res.json({ error: err, statusCode: response.statusCode });
       }
       tools.clearToken(req.session);
-      res.json({ response: "Revoke successful" });
+      res.json({ response: 'Revoke successful' });
     }
   );
 });
@@ -121,21 +133,21 @@ router.get("/revoke", function(req, res) {
 // Note: typical use case would be to refresh the tokens internally (not an API call)
 // We recommend refreshing upon receiving a 401 Unauthorized response from Intuit.
 // A working example of this can be seen above: `/api_call`
-router.get("/refresh", function(req, res) {
+router.get('/refresh', function(req, res) {
   let accessToken;
-  client.get("accessToken", (err, reply) => {
+  client.get('accessToken', (err, reply) => {
     accessToken = reply;
-    if (!accessToken) return res.json({ error: "Not authorized" });
+    if (!accessToken) return res.json({ error: 'Not authorized' });
   });
 
   let accessTokenFake, refreshToken, tokenType, data;
-  client.get("accessToken", (err, reply) => {
+  client.get('accessToken', (err, reply) => {
     accessTokenFake = reply;
-    client.get("refreshToken", (err, reply) => {
+    client.get('refreshToken', (err, reply) => {
       refreshToken = reply;
-      client.get("tokenType", (err, reply) => {
+      client.get('tokenType', (err, reply) => {
         tokenType = reply;
-        client.get("data", (err, reply) => {
+        client.get('data', (err, reply) => {
           data = reply;
 
           let fakeToken = {
